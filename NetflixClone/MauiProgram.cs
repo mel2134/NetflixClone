5354de54f36c8304f90edc7c769ec1c75ab499b8
Opilane TTHK <opilane@tthk.ee>
2025-05-12 12:30:20 +0300

    Initial commit

---
+++ NetflixClone/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using Services;
+
+namespace NetflixClone;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>().UseMauiCommunityToolkit()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Poppins-Regular.ttf", "PoppinsRegular");
+				fonts.AddFont("Poppins-Semibold.ttf", "PoppinsSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+		builder.Services.AddHttpClient(TmdbService.TmbdHttpClientName, httpClient => httpClient.BaseAddress = new Uri("https://api.themoviedb.org"));
+		builder.Services.AddSingleton<TmdbService>();
+		builder.Services.AddSingleton<MainPage>();
+
+		return builder.Build();
+	}
+}
